7433f2c6186d71772771eee7cf37f9ed5cf9cb11
nadaabdelmaboud <42664649+nadaabdelmaboud@users.noreply.github.com>
2021-06-06 22:30:40 +0200

    update

--- Master/config.js
+++
@@ -1,8 +0,0 @@
-const dbs = {
-    "master":"mongodb+srv://BigTable:BigTable@cluster0.xapub.mongodb.net/Master",
-    "tablet1":"mongodb+srv://BigTable:BigTable@cluster0.xapub.mongodb.net/Tablet1",
-    "tablet2":"mongodb+srv://BigTable:BigTable@cluster0.xapub.mongodb.net/Tablet2",
-    "tablet3":"mongodb+srv://BigTable:BigTable@cluster0.xapub.mongodb.net/Tablet3",
-}
-
-module.exports = dbs;
